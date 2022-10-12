dcbbcb3dea50e73e218b0cea75671b2b9ec6b025
Kelechi <kelechiuma@gmail.com>
2022-10-12 08:23:00 +0100

    Mongodb setup

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extented: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extented: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://kusjay:goatmeat123@development.xptf0.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 6000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
